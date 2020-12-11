98001765e0e2f5271fb3adbe685fcb49cea00701
Yasín Muñoz <yasincna@hotmail.com>
2020-12-11 19:32:08 +0100

    Leccion 4. Variables

---
+++ js-basico/valores.js
@@ -0,0 +1,18 @@
+var nombre = "Óscar";
+
+// declarar
+var edad;
+ 
+// inicializar
+edad = 30;
+
+edad
+
+var elementos = ["computadora", "celular"];
+
+var persona = {
+    nombre: "Diego",
+    edad: 30
+}
+
+ 
