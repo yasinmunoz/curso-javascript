65e3a3bf3d95d7031480faaaf89a01cb2b88a04c
Yasín Muñoz <yasincna@hotmail.com>
2020-12-11 19:51:17 +0100

    Leccion 5. Funciones

---
+++ js-basico/funciones.js
@@ -0,0 +1,16 @@
+// Funciones declarativas
+
+function miFuncion() {
+    return 3;
+}
+
+miFuncion();
+
+// Funciones de expresión o funciones anónimas
+
+var miFuncion = function(a,b) {
+    return a + b;
+}
+
+miFuncion();
+
