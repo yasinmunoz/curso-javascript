a340dc3dea5659db4ee618510efe4b9a596c9cec
Yasín Muñoz <yasincna@hotmail.com>
2020-12-11 20:04:07 +0100

    Leccion 6. Funciones ddeclarativas y de expresion. Hoisting

--- js-basico/funciones.js
+++ js-basico/funciones.js
@@ -6,6 +6,13 @@ function miFuncion() {
 
 miFuncion();
 
+function saludar (nombre) {
+    console.log ("Hola "+nombre);
+    // console.log (`Hola ${nombre}`);
+}
+
+saludar ("Diego");
+
 // Funciones de expresión o funciones anónimas
 
 var miFuncion = function(a,b) {
@@ -14,3 +21,14 @@ var miFuncion = function(a,b) {
 
 miFuncion();
 
+var nombre = function (nombre){
+    console.log (`Hola ${nombre}`);
+}
+
+nombre ("Diego");
+
+/* 
+
+A las funciones declarativas se les aplica hoisting y a las de expresión no, ya que el hoisting solo se aplica en las palabras reservadas var y function
+
+*/
