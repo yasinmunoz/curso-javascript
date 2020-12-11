6115bf0206e16f6daf31c62d0b9d7187c467fb7f
Yasín Muñoz <yasincna@hotmail.com>
2020-12-12 00:10:49 +0100

    JavaScript para prinicipiantes

---
+++ javascript-principiantes/app.js
@@ -0,0 +1,80 @@
+/*// Tipos de datos
+
+"Hello World"   // string
+'Hello World'   // también es un string
+
+1000000 // número
+- 2.3
+
+//boolean
+true 
+false
+
+//array
+["joe", "ryan", "martha"]
+[1,2,3]
+[true,false,true,false]
+
+// object
+{
+    "username":'ryan', 
+    "score":70.4, 
+    "hours":14, 
+    "professional":true,
+    "friends":["","",""];
+}
+
+{
+    "username":'joe', 
+    "score":0.4, 
+    "hours":1, 
+    "professional":false,
+    "friends":["","",""];
+}*/
+
+/*console.log (
+    {
+        "username":"ryan",
+        "score":70.4
+    }
+);*/
+
+
+/*Variables*/
+
+var nameUser = "john";
+let lastName = "carter";
+const PI = 3.14;
+
+nameUser = "yasin";
+console.log(PI);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
