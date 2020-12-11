6aa7449c0d8f999b084ab1c3679a007d64d8f0cc
Yasín Muñoz <yasincna@hotmail.com>
2020-12-11 22:24:32 +0100

    Creacion js-CheatSheet

---
+++ js-CheatSheet.js
@@ -0,0 +1,8 @@
+/* Variables */
+
+var a;                      //variable
+var b = "Hello World";      //string
+var c = "Hello " + "World"; //"Hello World"
+var d = 1 + 2 + "3" + 4;    //"1234"
+var e = [1,2,3,4];          //array
+var f = true;               //boolean
